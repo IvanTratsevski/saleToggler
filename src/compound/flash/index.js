2585ba082e9e08d73abd1d619604f468ba4d2005
IvanTratsevski <92509496+IvanTratsevski@users.noreply.github.com>
2022-08-21 19:07:52 +0300

    Update index.js

--- src/compound/flash/index.js
+++ src/compound/flash/index.js
@@ -1,28 +1,24 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import styles from "./index.module.css";
 
 
 
 export const Flash = ({children, startSaleStatus}) => {
-    const [alarmVisibleStatus,setAlarmVisibleStatus] = useState("hiden");
-    const flashingEffect = () =>{
-        const flasingInterval = setInterval(() => {
-            const flashingShow = setTimeout(() => {
-                setAlarmVisibleStatus("show");
-            }, 5000);
-            return () => {
-              clearTimeout(flashingShow);
-            };
-          }, 10000);
-    }
-    flashingEffect();
-    return (<div>
-        { startSaleStatus ? 
-            <div className={alarmVisibleStatus}>
-                {children}
-            </div> :
-            {children}
+    const [alarmVisibleStatus,setAlarmVisibleStatus] = useState(styles.hiden);
+        useEffect(() => {
+            const flashingInterval = setInterval(() => {
+                setAlarmVisibleStatus(styles.show)
+                const flashingIntervalHide = setTimeout(() => {
+                    setAlarmVisibleStatus(styles.hide);
+                },500)
+            }, 1000);
+            return() => {
+                clearInterval(flashingInterval);
+            }
         }
+        )
+    return (<div className={startSaleStatus && alarmVisibleStatus}>
+            {children}
         </div>
     );
 }
